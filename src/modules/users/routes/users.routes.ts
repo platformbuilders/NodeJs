57143a595d1932e528c44f323fa4bd48dc3b32a0
Leonardo Natal <leonardo_natal93@hotmail.com>
2021-03-21 20:33:12 -0300

    validations for each field

--- src/modules/users/routes/users.routes.ts
+++ src/modules/users/routes/users.routes.ts
@@ -1,13 +1,53 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UsersController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const usersRouter = Router();
 const userController = new UsersController();
 
 usersRouter.get('/', userController.index);
-usersRouter.get('/:id', userController.show);
-usersRouter.post('/', userController.create);
-usersRouter.put('/:id', userController.update);
-usersRouter.delete('/:id', userController.delete);
+
+usersRouter.get(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  userController.show);
+
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6)
+    },
+  }),
+  userController.create);
+
+usersRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().min(6)
+    },
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  userController.update);
+
+usersRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().uuid().required(),
+    },
+  }),
+  userController.delete);
 
 export default usersRouter;
