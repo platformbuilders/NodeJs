03391032004fa113ead5ce78fc6fbdce811e7b19
Leonardo Natal <leonardo_natal93@hotmail.com>
2021-03-22 17:43:27 -0300

    insercao de cryptografia

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -3,6 +3,7 @@ import { UserRepository } from './../typeorm/repositories/UsersRepository';
 import { getCustomRepository } from "typeorm";
 import User from '../typeorm/entities/User';
 import AppError from '@shared/errors/AppError';
+import { hash } from 'bcryptjs';
 
 interface IRequest {
   name: string;
@@ -11,16 +12,23 @@ interface IRequest {
 }
 
 class CreateUserService {
-  public async execute(data: IRequest): Promise<User> {
+  public async execute({name, email, password}: IRequest): Promise<User> {
     // utilizando um repo customizado
     const userRepository = getCustomRepository(UserRepository);
-    const userExists = await userRepository.findbyEmail(data.email);
+    const userExists = await userRepository.findbyEmail(email);
 
     if (userExists) {
       throw new AppError('Este email já foi cadastrado');
     }
 
-    const user = userRepository.create(data);
+    const hashedPassword = await hash(password, 8);
+
+    const user = userRepository.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
     await userRepository.save(user);
 
     return user;
