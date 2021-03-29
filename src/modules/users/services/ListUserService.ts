7a50ba13588a3f2c9dc0ac61ed5cadbc36b179e4
Leonardo Natal <leonardo_natal93@hotmail.com>
2021-03-29 20:15:01 -0300

    temos pagination

--- src/modules/users/services/ListUserService.ts
+++ src/modules/users/services/ListUserService.ts
@@ -3,14 +3,24 @@ import { UsersRepository } from '../typeorm/repositories/UsersRepository';
 import { getCustomRepository } from "typeorm";
 import User from '../typeorm/entities/User';
 
+interface IPagination {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: number | null;
+  next_page: number | null;
+  data: User[];
+}
 class ListUserService {
-  public async execute(): Promise<User[]> {
+  public async execute(): Promise<IPagination> {
 
     const userRepository = getCustomRepository(UsersRepository);
 
-    const user = userRepository.find();
+    const user = await userRepository.createQueryBuilder().paginate();
 
-    return user;
+    return user as IPagination;
   }
 }
 
